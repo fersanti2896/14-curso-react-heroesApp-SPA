abdcba1bf14bdf511e1fd3d85433b72da485748d
Fernando Nicolás <fersanti2896@gmail.com>
2023-02-15 16:56:38 -0600

    Uso del useNavigate en Navbar.jsx y LoginPage.jsx

--- src/ui/components/Navbar.jsx
+++ src/ui/components/Navbar.jsx
@@ -1,6 +1,14 @@
-import { Link, NavLink } from 'react-router-dom';
+import { Link, NavLink, useNavigate } from 'react-router-dom';
 
-export const Navbar = () => {
+export const Navbar = ( props ) => {
+    const navigate = useNavigate();
+
+    const onLogout = () => {
+        navigate('/login', {
+            replace: true
+        });
+    }
+
     return (
         <nav className='navbar navbar-expand-sm navbar-dark bg-dark p-2'>
             
@@ -26,11 +34,6 @@ export const Navbar = () => {
                              to="/search">
                         Buscador
                     </NavLink>
-
-                    <NavLink className={ ({ isActive }) => `nav-item nav-link ${ isActive ? 'active' : '' }` }
-                             to="/heroe">
-                        Heroe
-                    </NavLink>
                 </div>
             </div>
 
@@ -40,7 +43,8 @@ export const Navbar = () => {
                         Fernando
                     </span>
 
-                    <button className='nav-item nav-link btn'>
+                    <button className='nav-item nav-link btn'
+                            onClick={ onLogout }>
                         Logout
                     </button>
                 </ul>
